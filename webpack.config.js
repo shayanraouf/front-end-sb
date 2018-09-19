2a8363a747f6446438de0557e77c0c603babd3e3
Shayan Raouf <shayan.raouf@sap.com>
2018-09-19 15:19:14 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require("webpack");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: "public",
+		filename: "bundle.js",
+		publicPath: "/public/"
+	},
+	devServer: {
+		inline: true,
+		contentBase: './public',
+		port: 3000
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: ["babel-loader"],
+				query: {
+					presets: ["latest", "stage-0", "react"]
+				}
+			},
+			{
+				test: /\.json$/,
+				exclude: /(node_modules)/,
+				loader: "json-loader"
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader'
+			},
+			{
+				test: /\.scss$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+			}
+		]
+	}
+}
+
+
+
+
